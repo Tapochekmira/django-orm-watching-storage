7919b0598af0cb41223b90c1aab47f0a75c7fb2c
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-02-24 17:27:25 +0300

    made the display of visits by passcard

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,30 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
+
+from datacenter.models import (
+    Visit,
+    is_visit_long,
+    Passcard,
+    get_duration,
+    format_duration
+)
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    passcard = Passcard.objects.get(passcode=passcode)
+    visits = Visit.objects.filter(passcard=passcard)
+    this_passcard_visits = []
+    for visit in visits:
+        duration_at_second = get_duration(visit)
+        ready_to_output_duration = format_duration(duration_at_second)
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+        this_passcard_visits.append(
+            {
+                'entered_at': visit.entered_at,
+                'duration': ready_to_output_duration,
+                'is_strange': is_visit_long(visit)
+            }
+        )
+
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
