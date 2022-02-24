3b6d27945cfffc08eacb49262baffebe4400b6d8
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-02-24 17:28:00 +0300

    output person in storage

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,27 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
+
+from datacenter.models import (
+    Visit,
+    get_duration,
+    format_duration
+)
 
 
 def storage_information_view(request):
     # Программируем здесь
+    visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in visits:
+        duration_at_second = get_duration(visit)
+        ready_to_output_duration = format_duration(duration_at_second)
+        non_closed_visits.append(
+            {
+                'who_entered': visit.passcard.owner_name,
+                'entered_at': visit.entered_at,
+                'duration': ready_to_output_duration,
+            }
+        )
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
